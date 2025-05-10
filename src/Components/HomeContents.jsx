18ccfdfbe87abc7b090d8a372e57840c379b48f8
Karma <fauzikirahot@hotmail.com>
2025-05-10 21:35:51 +0700

    TEMPLATE

---
+++ src/Components/HomeContents.jsx
@@ -0,0 +1,9 @@
+export default function HomeContents() {
+  return (
+    <div>
+        <h1 className="flex justify-center items-center h-screen text-4xl font-bold text-red-600">
+            OKEH NGAN TES, TEXT WARNA BEUREUM.
+        </h1>
+    </div>
+  );
+}
