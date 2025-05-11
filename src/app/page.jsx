f6258a008042ffd5227d954e31a8a2fc5ed951fd
Karma <fauzikirahot@hotmail.com>
2025-05-11 22:25:59 +0700

    NAMBAHAN UPCOMING

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import HeaderContents from "@/Components/Main/HeaderContens";
 import BodyContents from "@/Components/Main/BodyContents";
 import FooterContents from "@/Components/Main/FooterContents";
+import HeaderAnnouncement from "@/Components/Main/HeaderAnnouncements";
 
 export default function Home() {
 	return (
 		<>
+			<HeaderAnnouncement />
 			<HeaderContents />
 			<BodyContents />
 			<FooterContents />
